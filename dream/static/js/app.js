001023c0ff1eca84079a82688178f3263c4a21d7
Michell Ayala <starships@outlook.com>
2016-04-07 21:06:14 -0500

    add: modal button

--- dream/static/js/app.js
+++ dream/static/js/app.js
@@ -3,9 +3,15 @@
 
   const button = document.querySelector('#donate-button');
   const modal = document.querySelector('.modal');
+  const close = document.querySelector('#close');
 
   button.addEventListener('click', (e) => {
     e.preventDefault();
     modal.classList.add('active');
+  });
+
+  close.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.remove('active');
   });
 }();
